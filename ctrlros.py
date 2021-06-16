be719b59a3c5e32f3674bbf26195d13abbd82ac9
kka-na <kimgana96@gmail.com>
2021-06-16 15:40:31 +0900

    210616

--- ctrlros.py
+++ ctrlros.py
@@ -4,19 +4,24 @@ from PyQt5.QtCore import pyqtSignal, QObject
 import rospy
 import rospkg
 import roslib
+import tf
 
 from sensor_msgs.msg import Image,CompressedImage
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
 
 class CtrlRos(QObject):
 	sendRP = pyqtSignal(float, float, float)
+	sendQE = pyqtSignal(object, object)
+
 	def __init__(self) :
 		super(CtrlRos, self).__init__()
 		rospy.init_node('visualizer', anonymous=True)
 		self.sub_topic()
 
 	def sub_topic(self) :
-		rospy.Subscriber('/pose', Odometry, self.imuCallBack)
+		rospy.Subscriber('/pose', Odometry, self.imuCallBack) #Odometry -> IMU
+
 
 	def imuCallBack(self, data) :
 		"""
@@ -24,14 +29,18 @@ class CtrlRos(QObject):
 		quaternion = (data.orientation.x,data.orientation.y, data.orientation.z,data.orientation.w  )
 		euler = tf.transformations.euler_from_quaternion(quaternion)	
 		PI = 3.141592
-		pitch = round(euler[0]*180/PI, 4)
-		roll = round(euler[1]*180/PI, 4)
-		yaw = round(euler[2]*180/PI, 4)
+		pitch = round(euler[0]*180/PI, 4) #y
+		roll = round(euler[1]*180/PI, 4) #x
+		yaw = round(euler[2]*180/PI, 4) #z
 		"""
+		
 		#test
 		roll = data.pose.pose.position.z
 		pitch = -data.pose.pose.position.z
 		yaw = 0.0
-		self.sendRP.emit(roll, pitch, yaw)
+		quaternion = (data.pose.pose.position.z,-data.pose.pose.position.z,data.pose.pose.position.z,-data.pose.pose.position.z  )
+		euler = tf.transformations.euler_from_quaternion(quaternion)	
 
+		self.sendRP.emit(roll, pitch, yaw)
+		self.sendQE.emit(quaternion, euler)
 
--- ngv.py
+++ ngv.py
@@ -9,6 +9,7 @@ from PyQt5 import uic
 
 
 import imugl
+import ctrlros
 
 form_class = uic.loadUiType("ngv.ui")[0]
 
@@ -19,8 +20,17 @@ class WindowClass(QMainWindow, form_class) :
 		self.setupUi(self)
 		self.imu_widget = imugl.ImuGL()
 		self.imu_layout.addWidget(self.imu_widget)
+		self.ctrlros = ctrlros.CtrlRos()
+		self.ctrlros.sendQE.connect(self.updateQE)
+
 		self.imu_widget.show()
 
+	@pyqtSlot(object, object )
+	def updateQE(self, quaternion, euler):
+		quat_txt = "x : {}  y : {}  z : {}  w : {}".format(round(quaternion[0],4), round(quaternion[1],4), round(quaternion[2],4), round(quaternion[3],4))
+		self.quat_label.setText(quat_txt)
+		eul_txt = "X : {}  Y : {}  Z : {}".format(round(euler[1],4), round(euler[0],4), round(euler[2],4))
+		self.eul_label.setText(eul_txt)
 
 if __name__ == "__main__" :
 	app = QApplication(sys.argv)
